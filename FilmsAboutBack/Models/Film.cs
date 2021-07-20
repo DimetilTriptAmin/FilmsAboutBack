0f24e8e8a20a484af850dc97cd4905cb5aa32bb3
Aliaksei Barushka <alekseyknows@gmail.com>
2021-07-20 14:41:40 +0300

    added film and rating model, removed 'model' folder, added everything in 'models'

---
+++ FilmsAboutBack/Models/Film.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FilmsAboutBack.Models
+{
+    public class Film
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public byte[] Poster { get; set; }
+        public string Description { get; set; }
+        public string TrailerLink { get; set; }
+        public int Rating { get; set; }
+
+    }
+}
---
+++ FilmsAboutBack/Models/Rating.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FilmsAboutBack.Models
+{
+    public class Rating
+    {
+        public int UserId { get; set; }
+        public int FilmId { get; set; }
+        public int Rate { get; set; }
+    }
+}
