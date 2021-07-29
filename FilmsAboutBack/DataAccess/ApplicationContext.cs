ebc4f325a180e22ca7bb332f1b880c3b0168783d
Дмитрий Карелин <d.v.karel1n@mail.ru>
2021-07-29 16:43:30 +0300

    fix seeding

--- FilmsAboutBack/DataAccess/ApplicationContext.cs
+++ FilmsAboutBack/DataAccess/ApplicationContext.cs
@@ -5,6 +5,7 @@ using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 using System;
 using System.IO;
+using System.Text;
 
 namespace FilmsAboutBack.DataAccess
 {
@@ -28,7 +29,7 @@ namespace FilmsAboutBack.DataAccess
                 Title = "Fight Club",
                 Description = "An insomniac office worker and a devil-may-care soap maker form an underground " +
                     "fight club that evolves into much more.",
-                Poster = Base64Coder.EncodeImg(Path.GetFullPath(@"../FilmsAboutBack/Assets/posters/fight_club.jpg")),
+                Poster = Encoding.ASCII.GetBytes(Base64Coder.EncodeImg(Path.GetFullPath(@"../FilmsAboutBack/Assets/posters/fight_club.jpg"))),
                 Rating = 4.7,
                 TrailerLink= "https://www.youtube.com/watch?v=O1nDozs-LxI&ab_channel=FilmFeed",
             });
@@ -38,7 +39,7 @@ namespace FilmsAboutBack.DataAccess
                 Id = 2,
                 Title = "Scarface",
                 Description = "In 1980 Miami, a determined Cuban immigrant takes over a drug cartel and succumbs to greed.",
-                Poster = Base64Coder.EncodeImg(Path.GetFullPath(@"../FilmsAboutBack/Assets/posters/scarface.jpg")),
+                Poster = Encoding.ASCII.GetBytes(Base64Coder.EncodeImg(Path.GetFullPath(@"../FilmsAboutBack/Assets/posters/scarface.jpg"))),
                 Rating = 4.8,
                 TrailerLink= "https://www.youtube.com/watch?v=7pQQHnqBa2E&ab_channel=FaceOff",
             });
@@ -50,7 +51,8 @@ namespace FilmsAboutBack.DataAccess
                 Description = "An American expat tries to sell off his highly profitable marijuana empire " +
                     "in London, triggering plots, schemes, bribery and blackmail in an attempt to steal his domain " +
                     "out from under him.",
-                Poster = Base64Coder.EncodeImg(Path.GetFullPath(@"../FilmsAboutBack/Assets/posters/the_gentlemen.jpg")),
+                Poster = Encoding.ASCII.GetBytes(Base64Coder.EncodeImg(Path.GetFullPath(@"../FilmsAboutBack/Assets/posters/the_gentlemen.jpg"))),
+
                 Rating = 4.5,
                 TrailerLink = "https://www.youtube.com/watch?v=Ify9S7hj480&ab_channel=MovieclipsTrailers"
             }); ;
@@ -61,7 +63,7 @@ namespace FilmsAboutBack.DataAccess
                 Title = "Pulp Fiction",
                 Description = "The lives of two mob hitmen, a boxer, a gangster and his wife, and a pair of diner " +
                     "bandits intertwine in four tales of violence and redemption.",
-                Poster = Base64Coder.EncodeImg(Path.GetFullPath(@"../FilmsAboutBack/Assets/posters/pulp_fiction.jpg")),
+                Poster = Encoding.ASCII.GetBytes(Base64Coder.EncodeImg(Path.GetFullPath(@"../FilmsAboutBack/Assets/posters/pulp_fiction.jpg"))),
                 Rating = 4.9,
                 TrailerLink = "https://www.youtube.com/watch?v=s7EdQ4FqbhY&ab_channel=Movieclips",
             });
@@ -72,7 +74,8 @@ namespace FilmsAboutBack.DataAccess
                 Title = "The Devil's Advocate",
                 Description = "An exceptionally adept Florida lawyer is offered a job at a high-end New York City " +
                     "law firm with a high-end boss - the biggest opportunity of his career to date.",
-                Poster = Base64Coder.EncodeImg(Path.GetFullPath(@"../FilmsAboutBack/Assets/posters/the_devils_advocate.jpg")),
+                Poster = Encoding.ASCII.GetBytes(Base64Coder.EncodeImg(Path.GetFullPath(@"../FilmsAboutBack/Assets/posters/the_devils_advocate.jpg"))),
+
                 Rating = 4.2,
                 TrailerLink= "https://www.youtube.com/watch?v=40hHA9n4C2o&ab_channel=MovieclipsClassicTrailers",
             });
--- FilmsAboutBack/DataAccess/Models/Film.cs
+++ FilmsAboutBack/DataAccess/Models/Film.cs
@@ -7,7 +7,7 @@ namespace FilmsAboutBack.Models
         public int Id { get; set; }
         public string Title { get; set; }
         [Column(TypeName = "varchar(max)")]
-        public string Poster { get; set; }
+        public byte[] Poster { get; set; }
         public string Description { get; set; }
         public string TrailerLink { get; set; }
         public double Rating { get; set; }
